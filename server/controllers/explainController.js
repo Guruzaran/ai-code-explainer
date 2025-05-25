291c2a728d441e81675ed04c785d3da6191187bd
Gurusaran V R <guruzaran@gmail.com>
2025-05-25 18:47:22 -0400

    Install and Integrate ollama - codellama

--- server/controllers/explainController.js
+++ server/controllers/explainController.js
@@ -1,5 +1,19 @@
+const axios  = require('axios');
 exports.explainCode = async (req, res) => {
     const { code } = req.body;
     if(!code) return res.status(400).json({error: 'No code provided'});
-    res.json({ explanation: `Explanation for:\n ${code}` });
+
+    const prompt = `Explain the following code in simple terms:\n\n${code}`;
+    try{
+        const response = await axios.post('http://localhost:11434/api/generate',{
+            model: 'codellama',
+            prompt,
+            stream: false
+        })
+        res.json({ explanation: response.data.response});
+    }
+    catch(error){
+        console.error('Ollama error:', error.message);
+        res.status(500).json({error: 'Failed to generate explanation'});
+    }
 };
